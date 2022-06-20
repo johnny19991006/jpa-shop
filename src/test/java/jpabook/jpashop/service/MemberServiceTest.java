11dd02012248b2ca172fbf88e04c254e259460a4
JunYoung Jang <police0023@naver.com>
2022-06-21 01:05:54 +0900

    second commit

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -36,8 +36,21 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(savedId));
 
     }
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception{
+        //given
+        Member member1=new Member();
+        member1.setName("Kim");
+
+        Member member2=new Member();
+        member2.setName("Kim");
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+        fail("예외가 발생해야 한다");
+
 
     }
 
